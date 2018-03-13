0bf1641100ba363e4a3317baa547192c6131ec89
Arapaut V Sivaprasad <avs@webgenie.com>
2018-03-13 17:39:39 +1100

    Committed_on_2018_03_13__17_39_32

--- contacts.py
+++ contacts.py
@@ -15,6 +15,7 @@ import sys
 from getpass import getpass
 from email_utils import EmailConnection, Email
 from secrets import Secret
+#import hashlib
 #-------------------------------------------------------------------------------
 # Globals
 #-------------------------------------------------------------------------------
@@ -24,13 +25,56 @@ to_email = Secret('to_email')
 password = Secret('pw')
 subject = 'Uploading and sending file'
 #-------------------------------------------------------------------------------
+# Upload a file
+#-------------------------------------------------------------------------------
+def uploadfile(form):
+	try: # Windows needs stdio set for binary mode.
+	    import msvcrt
+	    import uuid
+	    msvcrt.setmode (0, os.O_BINARY) # stdin  = 0
+	    msvcrt.setmode (1, os.O_BINARY) # stdout = 1
+	except ImportError:
+	    pass
+	
+	# Generator to buffer file chunks
+	def fbuffer(f, chunk_size=10000):
+	   while True:
+	      chunk = f.read(chunk_size)
+	      if not chunk: break
+	      yield chunk
+	
+	# A nested FieldStorage instance holds the file
+	fileitem = form['file']
+
+	# Test if the file was uploaded
+	if fileitem.filename:
+	
+		# strip leading path from file name to avoid directory traversal attacks
+		fn = os.path.basename(fileitem.filename)
+		
+		# Internet Explorer will attempt to provide full path for filename fix
+		fn = fn.split('\\')[-1]
+		
+		# This path must be writable by the web server in order to upload the file.
+		path = '/tmp/'
+		filepath = path + fn
+
+		# Open the file for writing 
+		f = open(filepath , 'wb', 10000)
+		
+		datalength = 0
+
+		# Read the file in chunks
+		for chunk in fbuffer(fileitem.file):
+			f.write(chunk)
+			datalength += len(chunk)
+		f.close()
+		return filepath
+#-------------------------------------------------------------------------------
 # Mail the form content with or without attachment
 #-------------------------------------------------------------------------------
-def mailit(name,email,phone,message_text,base_filename):
-#	global name,email,phone,message_text # Either export and import as global, or send as params
-	
-	if base_filename and base_filename != 'undefined': fileupload = '/var/www/vhosts/webgenie.com/httpdocs/Tmp/' + base_filename
-	else: fileupload = ''
+def mailit(name,email,phone,message_text,filepath):
+	fileupload = filepath
 	message = "<br><b>Name:</b> %s" % (name)
 	message = "%s<br><b>Email:</b> %s" % (message,email)
 	message = "%s<br><b>Phone:</b> %s" % (message,phone)
@@ -51,27 +95,25 @@ def mailit(name,email,phone,message_text,base_filename):
 		
 	server.send(email)
 	server.close()
-	print ("Content-type:text/html\n\n")
-	print("Your file (%s) has been uploaded and emailed." % base_filename)
+	print ("location: http://www.webgenie.com/thanks.html\n\n")
 #-------------------------------------------------------------------------------
 # Call the main to read the form inputs. Check if re-Captcha passed and, then, call 'mailit()'
 #-------------------------------------------------------------------------------
 def main():	
-#	global name,email,phone,message_text # Either export and import as global, or send as params
-	
-	# Create instance of FieldStorage. This will only work if CGI is in /cgi-bin 
 	form = cgi.FieldStorage() 
-
-	# Get data from fields
-	name = form.getvalue('Use_name')
-	email  = form.getvalue('User_email')
-	phone  = form.getvalue('User_phone')
-	message_text  = form.getvalue('Message')
-	base_filename  = form.getvalue('fileupload')
 	recaptcha  = form.getvalue('g-recaptcha-response')
-	if recaptcha: mailit(name,email,phone,message_text,base_filename)
+	if recaptcha:
+		filepath = uploadfile(form);
+		# Get data from fields
+		name = form.getvalue('Use_name')
+		email  = form.getvalue('User_email')
+		phone  = form.getvalue('User_phone')
+		message_text  = form.getvalue('Message')
+		mailit(name,email,phone,message_text,filepath)
+
 	else: print ("location: http://www.webgenie.com/nocaptcha.html\n\n")
 #-------------------------------------------------------------------------------
+#print ("Content-type:text/html\n\n")
 main()
 #-------------------------------------------------------------------------------
 
